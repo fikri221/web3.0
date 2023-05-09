091bfed61bd1ef3999d14cd97eb534edce589dfa
fikri.lazuardi <fikri.lazuardi@nds.co.id>
2023-05-09 22:21:19 +0700

    fix navbar and add icons

--- client/src/components/Navbar/NavbarItems.jsx
+++ client/src/components/Navbar/NavbarItems.jsx
@@ -9,7 +9,7 @@ const NavbarItem = ({ title, active, href }) => {
           active
             ? "text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:text-white md:dark:text-blue-500"
             : "text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:text-white md:dark:hover:bg-transparent"
-        } ${"md:hidden"}`}
+        }`}
         aria-current="page"
       >
         {title}
